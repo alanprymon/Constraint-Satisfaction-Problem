ebe4fee4456f6d7ff28849a529b9fa2ba29132a2
Alan Prymon <alanprymon@gmail.com>
2024-10-29 01:11:26 -0500

    helper functions

--- cs480_P02_A20483983.py
+++ cs480_P02_A20483983.py
@@ -26,7 +26,7 @@ def get_parks() -> list:
     #print(parks)
     return parks
 
-def get_distances() -> (list, list):
+def get_distances_matrix() -> (list, list):
     file = open('driving2.csv', 'rt')
     axis = file.readline().replace('\n', '').split(',')
     axis.pop(0)
@@ -38,6 +38,28 @@ def get_distances() -> (list, list):
     # print(axis)
     # print(distances)
     return axis, distances
+
+def get_distance(at: str, to: str, distances: (list,list)) -> int:
+    axis = distances[0]
+    distances = distances[1]
+    for i in range(len(axis)):
+        if axis[i] == at:
+            at_index = i
+        if axis[i] == to:
+            to_index = i
+    return int(distances[at_index][to_index])
+
+def get_possible_next(at:str, zones: list, distances: (list, list)) -> list:
+    for i in range(len(zones[0])):
+        if at == zones[0][i]:
+            at_zone = int(zones[1][i])
+    possible = []
+    for i in range(len(zones[1])):
+        if at_zone + 1 == int(zones[1][i]):
+            if get_distance(at, zones[0][i], distances) > -1:
+                possible.append(zones[0][i])
+    return possible
+
 
 if __name__ == '__main__':
     if not len(sys.argv) == 3:#checking that the number of arguements is correct
@@ -56,8 +78,10 @@ if __name__ == '__main__':
 
     zones = get_zones()
     parks = get_parks()
-    axis, distances = get_distances()
+    distance_matrix = get_distances_matrix()
+    print(str(get_distance('AZ', 'NV', distance_matrix)))
+    print(get_possible_next('AZ', zones, distance_matrix))
 
-    
 
-    print("Prymon, Alan, A20483983 solution:\nInitial state: " + initial + "\nMinimum number of parks: " + num_of_parks)
+
+    # print("Prymon, Alan, A20483983 solution:\nInitial state: " + initial + "\nMinimum number of parks: " + num_of_parks)
